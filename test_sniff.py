5d7a223c37302d418578572ca3cafe1fdb4769ac
EL-Moutouk Mohamed Yassir <elmoutoukmohamedyassir@gmail.com>
2026-03-16 16:55:46 +0000

    cleanup the code

--- test_sniff.py
+++ test_sniff.py
@@ -4,5 +4,5 @@ def simple_callback(packet):
     print(f"Packet Detected! Protocol: {packet.summary()}")
 
 print("Starting sniffer... (Press Ctrl+C to stop)")
-# This captures 10 packets
+
 sniff(prn=simple_callback, count=10)
